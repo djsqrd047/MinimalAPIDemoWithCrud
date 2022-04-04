cbc829aae740d94b226fe509aa8729fe3e360f03
David Jones <dajones@quiktrip.com>
2022-04-04 13:03:45 -0500

    Initial

--- MinimalAPIDemoWithCrud/Program.cs
+++ MinimalAPIDemoWithCrud/Program.cs
@@ -10,8 +10,7 @@ using MinimalAPIDemoWithCrudServices.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
